a0b68fadd18be29ad7db7ac644696075ab6e5fa3
Lekshmi <sreelekshmi310192@gmail.com>
2024-12-10 19:49:22 +0530

    added persist redux state logics

--- frontend/src/Redux/store.ts
+++ frontend/src/Redux/store.ts
@@ -1,11 +1,28 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from "./slices/authSlice"
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+import { combineReducers } from 'redux';
+
+// Combine reducers 
+const rootReducer = combineReducers({
+  auth: authReducer,
+});
+
+// Persist configuration
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    auth: authReducer, // Add auth reducer
-  },
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
 
 // TypeScript types for store
 export type RootState = ReturnType<typeof store.getState>;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,13 +3,15 @@ import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from "./Redux/store"
+import { store, persistor } from "./Redux/store"
+import { PersistGate } from 'redux-persist/integration/react';
+
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Provider store={store}>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <App />
-    </Provider>
-  </React.StrictMode>
+    </PersistGate>
+  </Provider>
 );
