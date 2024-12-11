884f80d8c07725e8f334a7edc8da4d51d3fb47bd
Lekshmi <sreelekshmi310192@gmail.com>
2024-12-11 17:22:00 +0530

    docEase font style changed

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -6,15 +6,19 @@ const Header: React.FC = () => {
     <header className="w-full bg-customTeal flex items-center justify-between p-4">
       <div>
         <div className="flex items-center space-x-2">
-          <div className="text-2xl font-bold">DocEase</div>
-          <i className="fas fa-stethoscope text-white text-xl"></i>
+          <div
+            className="text-5xl" style={{ fontFamily: "'Lobster', cursive" }}
+          >
+            DocEase
+          </div>
+          <i className="fas fa-stethoscope text-white text-4xl"></i>
         </div>
         <div className="text-sm mt-1">Connect and Manage Your Patients Here</div>
       </div>
       <Link to="/doctor/signup">
         <button className="bg-[#9fc7cf] text-black font-semibold px-4 py-2 rounded-md hover:opacity-90 transition">
           Create An Account
-        </button>      
+        </button>
       </Link>
     </header>
   );
