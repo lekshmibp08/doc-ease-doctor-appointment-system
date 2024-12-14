4e5078771639ca5c5afaf6fd1017e354fdfea1ae
Lekshmi <sreelekshmi310192@gmail.com>
2024-12-14 22:03:51 +0530

    User profile UI completed

---
+++ frontend/src/pages/userPages/ProfilePage.tsx
@@ -0,0 +1,16 @@
+import UserHeader from '../../components/UserHeader'
+import UserProfile from '../../components/UserProfile'
+import Footer from '../../components/Footer'
+
+const ProfilePage = () => {
+  return (
+    <>
+        <UserHeader/>
+        <UserProfile/>
+        <Footer/>
+      
+    </>
+  )
+}
+
+export default ProfilePage
