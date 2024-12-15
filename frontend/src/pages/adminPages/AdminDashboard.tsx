4ed1a29b373ade7af836aa6fd979c5c42b9bf069
Lekshmi <sreelekshmi310192@gmail.com>
2024-12-15 12:24:38 +0530

    Demo admin dashboard completed

--- frontend/src/pages/adminPages/AdminDashboard.tsx
+++ frontend/src/pages/adminPages/AdminDashboard.tsx
@@ -1,19 +1,31 @@
-import useLogout from '../../hooks/useLogout';
+import { useState } from 'react';
+import AdminHeader from '../../components/AdminHeader';
+import Sidebar from '../../components/Sidebar';
+import Footer from '../../components/Footer';
 
 const AdminDashboard = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-    const logout = useLogout();    
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
 
-    return (<>
-        <h1>ADMIN DASHBOARD</h1>
-        <button
-            onClick={logout}
-            className="bg-red-500 text-white p-2 rounded"
-          >
-            Logout
-          </button>
-    
-    </>)
-}
+  return (
+    <>
+      <AdminHeader toggleSidebar={toggleSidebar} />
+      <div className="flex">
+        {/* Sidebar */}
+        <Sidebar isOpen={isSidebarOpen} />
+        {/* Main Content */}
+        <div className="flex-1 bg-gray-100 p-6">
+          
+          <h2 className="text-2xl font-bold">Welcome to Admin Dashboard</h2>
+          
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-export default AdminDashboard
+export default AdminDashboard;
