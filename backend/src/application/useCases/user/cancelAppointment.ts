f5bae2ee487b3fb62a704b7d6b35412c4aec85f7
Lekshmi <sreelekshmi310192@gmail.com>
2025-01-05 22:12:44 +0530

    updated cancel appoinment backend logic

--- backend/src/application/useCases/user/cancelAppointment.ts
+++ backend/src/application/useCases/user/cancelAppointment.ts
@@ -1,9 +1,11 @@
 import { IAppointmentRepository } from "../../../domain/repositories/IAppointmentRepository";
 import { IAppointment } from "../../../domain/entities/Appoinment";
+import { ISlotRepository } from "../../../domain/repositories/ISlotRepository";
 
 export const cancelAppointmentByUserUsecase = async (
     appointmentId: string,
-    appointmentRepository: IAppointmentRepository
+    appointmentRepository: IAppointmentRepository,
+    slotRepository: ISlotRepository
 ): Promise<any> => {
   try {
     const appointment = await appointmentRepository.findAppointmentsById(appointmentId);
@@ -12,6 +14,14 @@ export const cancelAppointmentByUserUsecase = async (
     }
     const updates = { isCancelled : true };
     const updatedAppointment = await appointmentRepository.updateAppointment(appointmentId, updates);
+
+    const slotId = appointment.slotId as string;
+    const timeSlotId = appointment.timeSlotId.toString().replace(/ObjectId\("(.*)"\)/, '$1');
+    const status = "Not Booked";
+
+    const updatedSlot = await slotRepository.updateSlotStatus(slotId, timeSlotId, status)
+    console.log(updatedSlot);
+    
     return updatedAppointment?.isCancelled;
     
   } catch (error) {
