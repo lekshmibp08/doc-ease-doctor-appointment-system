84da804d8d619d57b0b497270441dfc6f42d71d1
Lekshmi <sreelekshmi310192@gmail.com>
2025-02-25 13:39:04 +0530

    Added error middleware

---
+++ backend/src/infrastructure/middlewares/ErrorMiddleWare .ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Request, Response, NextFunction } from "express";
+
+interface CustomError extends Error {
+  status?: number;
+}
+
+// 404 Not Found Middleware
+export const notFound = (req: Request, res: Response, next: NextFunction) => {
+    res.status(404).json({ success: false, message: "API route not found" });
+};
+
+// Global Error Handling Middleware
+export const errorHandler = (err: CustomError, req: Request, res: Response, next: NextFunction) => {
+    console.error(err.stack);
+    const statusCode = err.status || 500;
+    res.status(statusCode).json({
+        success: false,
+        message: err.message || "Internal Server Error",
+    });
+};
