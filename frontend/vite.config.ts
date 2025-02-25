330259415895d1bad017a6a581c4d9d2f7661855
Lekshmi <sreelekshmi310192@gmail.com>
2025-02-25 12:36:49 +0530

    fixed scrollbar related issues

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-//https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
 
   server: {
     proxy: { 
-      "/api":{
-      target: "http://localhost:5000",
-      secure:false,
-      changeOrigin: true,
-    } },
+      "/api": {
+        target: "http://localhost:5000",
+        secure: false,
+        changeOrigin: true,
+      },
+    },
   },
-})
+
+  resolve: {
+    alias: {
+      stream: "stream-browserify",
+    },
+  },
+
+  define: {
+    global: "window", // Fixes the "global is not defined" issue
+  },
+});
