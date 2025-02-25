704de5ef997f13661743d6cc30386a82a0856e47
Lekshmi <sreelekshmi310192@gmail.com>
2025-02-25 19:00:00 +0530

    Unused React import removed

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store, persistor } from "./Redux/store";
 import { PersistGate } from 'redux-persist/integration/react';
-import React from 'react';
 
 
 
