436d3ff93973a55f2144b13892601cdde35dc2cb
Lekshmi <sreelekshmi310192@gmail.com>
2025-04-25 20:56:23 +0530

    updated eslint config for wrn unused imports

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,6 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import pluginUnusedImports from "eslint-plugin-unused-imports";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -11,9 +12,22 @@ export default [
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
+    plugins: {
+      unusedImports: pluginUnusedImports, // 👈 Register plugin
+    },
     rules: {
       "react/react-in-jsx-scope": "off", // ✅ Disables the outdated rule
       "@typescript-eslint/no-explicit-any": "off",
+      "unused-imports/no-unused-imports": "warn",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
     },
   },
 ];
