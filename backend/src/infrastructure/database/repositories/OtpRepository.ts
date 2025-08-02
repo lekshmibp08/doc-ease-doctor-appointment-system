b3408dee9c573f538bfcb564ef433153d9a344b4
Lekshmi <sreelekshmi310192@gmail.com>
2025-08-02 22:17:17 +0530

    otp and user repo converted to class

--- backend/src/infrastructure/database/repositories/OtpRepository.ts
+++ backend/src/infrastructure/database/repositories/OtpRepository.ts
@@ -2,15 +2,18 @@ import OtpModel from "../models/OtpModel";
 import { IOtpRepository } from "../../../domain/repositories/IOtpRepository";
 import { Otp } from "../../../domain/entities/Otp";
 
-export const createOtpRepository = (): IOtpRepository => ({
-  saveOtp: async (otp) => {
+export class OtpRepository implements IOtpRepository {
+  async saveOtp (otp: Otp): Promise<void> {
     await OtpModel.create(otp);
-  },
-  findOtp: async (email, otp) => {
+  }
+  async findOtp(email: string, otp: string): Promise<Otp | null> {
     const otpDoc = await OtpModel.findOne({ email, otp });
     return otpDoc ? { email: otpDoc.email, otp: otpDoc.otp, expiresAt: otpDoc.expiresAt } : null;
-  },
-  deleteOtp: async (email) => {
+  }
+  async deleteOtp(email: string): Promise<void> {
     await OtpModel.deleteOne({ email });
-  },
-});
+  }
+
+}
+
+
--- backend/src/infrastructure/database/repositories/UserRepository.ts
+++ backend/src/infrastructure/database/repositories/UserRepository.ts
@@ -1,36 +1,36 @@
 import { IUserRepository } from "../../../domain/repositories/IUserRepository";
 import UserModel from "../models/UserModel";
+import { User as IUser } from "../../../domain/entities/User";
 
-export const createUserRepository = (): IUserRepository => ({
-  findByEmail: async (email) => {
-    const userDoc = await UserModel.findOne({ email });
-    return userDoc;
-  },
-  create: async (user) => {
-    const userDoc = await UserModel.create(user);
-    return userDoc;
-  },
-  getAllUsers: async () => {
-    return await UserModel.find({ role: "user" }, "-password"); 
-  },
-  getUsersWithPagination: async (skip, limit, query) => {
+export class UserRepository implements IUserRepository {
+  async findByEmail(email: string): Promise<IUser | null> {
+    return await UserModel.findOne({ email });
+  }
+  async create(user: IUser): Promise<IUser> {
+    return await UserModel.create(user);
+  }
+  async getAllUsers(): Promise<IUser[]> {
+    return await UserModel.find({ role: "user" }, "-password");
+  }
+  async getUsersWithPagination(skip: number, limit: number, query: any): Promise<IUser[]> {
     return await UserModel.find({ ...query, role: "user" }, "-password")
       .skip(skip)
       .limit(limit)
-      .sort({ createdAt: -1 }); 
-  },
+      .sort({ createdAt: -1 });
+  }
 
-  countUsers: async (query) => {
-    return await UserModel.countDocuments(query); 
-  },  
-  findUserById: async (id) => {
+  async countUsers(query: any): Promise<number> {
+    return await UserModel.countDocuments(query);
+  }
+
+  async findUserById(id: string): Promise<IUser | null> {
     return await UserModel.findById(id);
-  },
-  updateUser: async (id, updates) => {
-    const updatedUser = await UserModel.findByIdAndUpdate(
+  }
+  async updateUser(id: string, updates: Partial<IUser>): Promise<IUser | null> {
+    return await UserModel.findByIdAndUpdate(
       id, updates, 
       { new: true}
     ).select('-password');
-    return updatedUser;
+    
   }
-});
+}
