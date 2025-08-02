e412940fb219a466228e57e49f4405567c56ee40
Lekshmi <sreelekshmi310192@gmail.com>
2025-08-02 22:32:11 +0530

    updated user model

--- backend/src/infrastructure/database/models/UserModel.ts
+++ backend/src/infrastructure/database/models/UserModel.ts
@@ -1,79 +1,68 @@
-import mongoose, { Schema, model } from "mongoose";
-import { User } from "../../../domain/entities/User";
+import mongoose from "mongoose";
 
-interface IUserDocument extends Document, Omit<User, "_id"> {
-    _id: mongoose.Types.ObjectId; 
-    profilePicture: string;
-    gender: string;
-    age: string;
-    addressline: string;
-    city: string;
-    state: string;
-    pincode: string;
-    createdAt: Date;
-    updatedAt: Date;
-}
-
-const UserSchema = new Schema<IUserDocument>(
-    {
-        fullName: {
-            type: String,
-            required: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        mobileNumber: {
-            type: String,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-        role: {
-            type: String,
-            enum: ["user", "doctor", "admin"],
-            default: "user",
-            required: true,
-        },
-        profilePicture: {
-          type: String,
-          default: 'https://www.pngkit.com/png/detail/126-1262807_instagram-default-profile-picture-png.png',          
-        },
-        gender: {
-          type: String,
-          default: "",
-        },
-        age: {
-          type: String,
-          default: "",
-        },
-        addressline: {
-          type: String,
-          default: "",
-        },
-        city: {
-          type: String,
-          default: "",
-        },
-        state: {
-          type: String,
-          default: "",
-        },
-        pincode: {
-          type: String,
-          default: "",
-        },
-        isBlocked: {
-            type: Boolean,
-            default: false,
-        },
+const UserSchema = new mongoose.Schema(
+  {
+    fullName: {
+        type: String,
+        required: true,
     },
-    {
-        timestamps: true, // Automatically adds createdAt and updatedAt fields
-    }
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    mobileNumber: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: ["user", "doctor", "admin"],
+        default: "user",
+        required: true,
+    },
+    profilePicture: {
+      type: String,
+      default: 'https://www.pngkit.com/png/detail/126-1262807_instagram-default-profile-picture-png.png',          
+    },
+    gender: {
+      type: String,
+      default: "",
+    },
+    age: {
+      type: String,
+      default: "",
+    },
+    addressline: {
+      type: String,
+      default: "",
+    },
+    city: {
+      type: String,
+      default: "",
+    },
+    state: {
+      type: String,
+      default: "",
+    },
+    pincode: {
+      type: String,
+      default: "",
+    },
+    isBlocked: {
+        type: Boolean,
+        default: false,
+    },
+  },
+  {
+      timestamps: true,
+  }
 );
 
-export default model<IUserDocument>("User", UserSchema);
+const User = mongoose.model("User", UserSchema);
+
+export default User;
