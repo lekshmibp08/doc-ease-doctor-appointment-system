182493e29bb89ed29a1e5d306ccbb660171151e4
Lekshmi <sreelekshmi310192@gmail.com>
2025-08-02 22:31:42 +0530

    verify otp function changed to class

--- backend/src/application/useCases/user/verifyOtpAndRegisterUser.ts
+++ backend/src/application/useCases/user/verifyOtpAndRegisterUser.ts
@@ -3,22 +3,25 @@ import { IUserRepository } from "../../../domain/repositories/IUserRepository";
 import { User } from "../../../domain/entities/User";
 import bcrypt from "bcrypt";
 
-export const verifyOtpAndRegister = async (
-  otpRepository: IOtpRepository,
-  userRepository: IUserRepository,
-  data: { email: string; otp: string; fullName: string; mobileNumber: string; password: string }
-): Promise<User> => {
-  const { email, otp, fullName, mobileNumber, password } = data;
+export class VerifyOtpAndRegister {
+  constructor(private otpRepository: IOtpRepository, private userRepository: IUserRepository,){}
 
-  const otpEntity = await otpRepository.findOtp(email, otp);
-  if (!otpEntity || new Date() > otpEntity.expiresAt) {
-    throw new Error("Invalid or expired OTP");
+  async execute (
+    data: { email: string; otp: string; fullName: string; mobileNumber: string; password: string }
+  ): Promise<User> {
+    const { email, otp, fullName, mobileNumber, password } = data;
+  
+    const otpEntity = await this.otpRepository.findOtp(email, otp);
+    if (!otpEntity || new Date() > otpEntity.expiresAt) {
+      throw new Error("Invalid or expired OTP");
+    }
+  
+    await this.otpRepository.deleteOtp(email);
+  
+    const hashedPassword = await bcrypt.hash(password, 10);
+  
+    const user: User = { fullName, email, mobileNumber, password: hashedPassword, role: "user", isBlocked: false };
+    return await this.userRepository.create(user);
   }
+}
 
-  await otpRepository.deleteOtp(email);
-
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  const user: User = { fullName, email, mobileNumber, password: hashedPassword, role: "user", isBlocked: false };
-  return await userRepository.create(user);
-};
