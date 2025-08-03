84e9d403a2d6e7b7c255e4c37db6f3c5feb86f22
Lekshmi <sreelekshmi310192@gmail.com>
2025-08-03 22:20:56 +0530

    update slot use case changed to class

--- backend/src/application/useCases/doctor/updateSlotUseCase.ts
+++ backend/src/application/useCases/doctor/updateSlotUseCase.ts
@@ -1,28 +1,30 @@
 import { ISlotRepository } from "../../../domain/repositories/ISlotRepository";
 
-export const updateSlotUseCase = async (
-  slotRepository: ISlotRepository,
-  slotId: string,
-  timeSlotId: string,
-  status: string
-): Promise<any> => {
-  let updation: boolean;
+export class UpdateSlotUseCase {
+  constructor(private slotRepository: ISlotRepository) {}
 
-  if (status === "Unavailable") {
-    updation = false;
-  } else if (status === "Available") {
-    updation = true;
-  } else {
-    throw new Error("Invalid status value");
+  async execute(
+    slotId: string,
+    timeSlotId: string,
+    status: string
+  ): Promise<any> {
+    let updation: boolean;
+
+    if (status === "Unavailable") {
+      updation = false;
+    } else if (status === "Available") {
+      updation = true;
+    } else {
+      throw new Error("Invalid status value");
+    }
+
+    const updatedSlot = await this.slotRepository.findByIdAndUpdateAvailability(
+      slotId,
+      timeSlotId,
+      updation
+    );
+    console.log("USECASE UPDATED SLOT: ", updatedSlot);
+
+    return { updatedSlot, updation };
   }
-
-  const updatedSlot = await slotRepository.findByIdAndUpdateAvailability(
-    slotId,
-    timeSlotId,
-    updation
-  );
-  console.log("USECASE UPDATED SLOT: ", updatedSlot);
-  
-
-  return {updatedSlot, updation}; 
-};
+}
--- backend/src/interfaces/controllers/SlotController.ts
+++ backend/src/interfaces/controllers/SlotController.ts
@@ -1,18 +1,16 @@
 import { Request, Response } from "express";
 import { SlotRepository } from "../../infrastructure/database/repositories/SlotRepository";
-import { updateSlotUseCase } from "../../application/useCases/doctor/updateSlotUseCase";
+import { UpdateSlotUseCase } from "../../application/useCases/doctor/updateSlotUseCase";
 import { fetchSlotUseCase } from "../../application/useCases/user/fetchSlotUseCase";
 import { updateSlotTimeUseCase } from "../../application/useCases/doctor/updateSlotTimeUseCase";
 import SlotUseCase from "../../application/useCases/SlotUseCase";
 
-//type TimePeriod = "Morning" | "Afternoon" | "Evening";
-
 const slotRepository = new SlotRepository();
+const updateSlotUseCase = new UpdateSlotUseCase(slotRepository);
 
 export const slotController = {
-
-  async generateSlots (req: Request, res: Response): Promise<void> {
-    try {      
+  async generateSlots(req: Request, res: Response): Promise<void> {
+    try {
       await SlotUseCase.generateSlots(req.body);
       res.status(201).json({ message: "Slots generated successfully!" });
     } catch (error) {
@@ -21,12 +19,14 @@ export const slotController = {
     }
   },
 
-  async fetchSlot (req: Request, res: Response): Promise<void> {        
-
+  async fetchSlot(req: Request, res: Response): Promise<void> {
     try {
-      console.log("Received params:", req.query); 
-      const { filteredSlots, slotDataAll, slotId } = await SlotUseCase.fetchSlots( req.query);
-      res.status(200).json({ slotDataFiltered: filteredSlots, slotDataAll, slotId });
+      console.log("Received params:", req.query);
+      const { filteredSlots, slotDataAll, slotId } =
+        await SlotUseCase.fetchSlots(req.query);
+      res
+        .status(200)
+        .json({ slotDataFiltered: filteredSlots, slotDataAll, slotId });
     } catch (error) {
       console.error("Error:", error);
       res.status(500).json({ message: "Internal server error" });
@@ -36,36 +36,58 @@ export const slotController = {
   updateSlotStatus: async (req: Request, res: Response): Promise<void> => {
     const { slotId, timeSlotId, status } = req.body;
     console.log("REQ BODY:", slotId, timeSlotId, status);
-    
+
     try {
-      const { updation } = await updateSlotUseCase(slotRepository, slotId, timeSlotId, status);
-      res.status(200).json({ message: "Slot status updated successfully.", updation });
+      const { updation } = await updateSlotUseCase.execute(
+        slotId,
+        timeSlotId,
+        status
+      );
+      res
+        .status(200)
+        .json({ message: "Slot status updated successfully.", updation });
     } catch (error: any) {
       console.error("Error updating slot status:", error);
-      res.status(400).json({ message: error.message || "Failed to update slot status." });
+      res
+        .status(400)
+        .json({ message: error.message || "Failed to update slot status." });
     }
   },
 
   fetchSlotsForUser: async (req: Request, res: Response): Promise<void> => {
     const { doctorId } = req.params;
-    const date = req.query.date as string;    
+    const date = req.query.date as string;
     try {
-      const existingSlot = await fetchSlotUseCase(slotRepository, doctorId, date);
-      res.status(200).json({ timeSlots: existingSlot?.timeSlots, slotId: existingSlot?._id });
+      const existingSlot = await fetchSlotUseCase(
+        slotRepository,
+        doctorId,
+        date
+      );
+      res.status(200).json({
+        timeSlots: existingSlot?.timeSlots,
+        slotId: existingSlot?._id,
+      });
     } catch (error) {
       console.error("Error:", error);
-      res.status(500).json({ message: "Internal server error" });      
+      res.status(500).json({ message: "Internal server error" });
     }
   },
 
   updateSlotTime: async (req: Request, res: Response): Promise<void> => {
     const { slotId, timeSlotId, newTime } = req.body;
-    
+
     try {
-      const success = await updateSlotTimeUseCase(slotRepository, slotId, timeSlotId, newTime);
+      const success = await updateSlotTimeUseCase(
+        slotRepository,
+        slotId,
+        timeSlotId,
+        newTime
+      );
       res.status(200).json({ success });
-    } catch (error:any) {
-      res.status(500).json({ error: error.message || "Failed to update slot time" });
+    } catch (error: any) {
+      res
+        .status(500)
+        .json({ error: error.message || "Failed to update slot time" });
     }
   },
 };
