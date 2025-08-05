cafa7e9a2d018787c9ffd9810436be6a3a5488f3
Lekshmi <sreelekshmi310192@gmail.com>
2025-08-05 14:25:35 +0530

    entities converted to class

--- backend/src/domain/entities/Doctor.ts
+++ backend/src/domain/entities/Doctor.ts
@@ -1,14 +1,28 @@
-import { Types } from "mongoose";
-
-export interface Doctor {
-  _id?: Types.ObjectId;
-  fullName: string;
-  email: string;
-  profilePicture?: string;
-  mobileNumber: string;
-  registerNumber: string;
-  password: string;
-  role: "doctor";
-  isApproved: boolean;
-  isBlocked: boolean;
-};
+export class Doctor {
+  constructor(
+    public fullName: string,
+    public email: string,
+    public password: string,
+    public role: "doctor",
+    public isApproved: boolean,
+    public isBlocked: boolean,
+    public _id?: string, 
+    public profilePicture?: string,
+    public mobileNumber?: string,
+    public registerNumber?: string,
+    public isRejected?: boolean,
+    public specialization?: string,
+    public qualification?: string,
+    public fee?: number,
+    public gender?: string,
+    public experience?: number,
+    public modesOfConsultation?: ("Video" | "Clinic")[],
+    public gallery?: string[],
+    public documents?: string[],
+    public locationCoordinates?: { latitude: number; longitude: number } | undefined,
+    public locationName?: string,
+    public addressLine?: string,
+    public createdAt?: Date,
+    public updatedAt?: Date
+  ) {}
+}
--- backend/src/domain/entities/User.ts
+++ backend/src/domain/entities/User.ts
@@ -1,12 +1,19 @@
-import { Types } from "mongoose";
 
-export interface IUser {
-  fullName: string;
-  email: string;
-  mobileNumber: string;
-  password: string;
-  role: "user" | "doctor" | "admin";
-  isBlocked: boolean;
-  _id?: Types.ObjectId;
-  profilePicture?: string;
+export class IUser {
+  constructor(
+    public fullName: string,
+    public email: string,
+    public mobileNumber: string,
+    public password: string,
+    public role: "user" | "doctor" | "admin",
+    public isBlocked: boolean,
+    public _id?: string,
+    public profilePicture?: string,
+    public gender?: string,      
+    public age?: string,
+    public addressline?: string,
+    public city?: string,
+    public state?: string,
+    public pincode?: string,
+  ){}
 }
