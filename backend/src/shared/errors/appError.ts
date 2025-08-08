b51bfeed74626120ddac910091b0de2eeaf782a7
Lekshmi <sreelekshmi310192@gmail.com>
2025-08-08 22:12:35 +0530

    custom error class implimented

---
+++ backend/src/shared/errors/appError.ts
@@ -0,0 +1,10 @@
+export class AppError extends Error {
+  status: number;
+  
+  constructor(message: string, status = 500) {
+    super(message);
+    this.status = status;
+    Object.setPrototypeOf(this, new.target.prototype); 
+    Error.captureStackTrace(this);
+  }
+}
