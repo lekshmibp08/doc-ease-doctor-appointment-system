8d826d666c1145fc8210c836c9c87a3442dde98a
Lekshmi <sreelekshmi310192@gmail.com>
2025-08-09 20:19:33 +0530

    type error fixed

--- backend/src/application/helper/stripBaseUrl.ts
+++ backend/src/application/helper/stripBaseUrl.ts
@@ -2,8 +2,8 @@
 const COMMON_STORAGE_URL = process.env.COMMON_STORAGE_URL || "";
 const GOOGLE_IMAGE_BASE_URL = process.env.GOOGLE_IMAGE_BASE_URL || "";
 
-export const stripBaseUrl = (url?: string): string | undefined => {
-  if (!url) return url;
+export const stripBaseUrl = (url?: string): string => {
+  if (!url) return '';
 
   if (url.startsWith(COMMON_STORAGE_URL)) {
     return `cl:${url.replace(COMMON_STORAGE_URL, "")}`;
