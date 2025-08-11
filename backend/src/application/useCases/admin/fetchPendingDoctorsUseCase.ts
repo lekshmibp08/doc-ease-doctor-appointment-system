ce91a5e5ceca1215e0f085ae6286a922c50f0bb2
Lekshmi <sreelekshmi310192@gmail.com>
2025-08-11 14:49:24 +0530

    pending doctors  list mapping done

--- backend/src/application/useCases/admin/fetchPendingDoctorsUseCase.ts
+++ backend/src/application/useCases/admin/fetchPendingDoctorsUseCase.ts
@@ -2,6 +2,17 @@ import { IDoctorRepository } from "../../../domain/repositories/IDoctorRepositor
 
 export class FetchPendingDoctors {
   constructor(private doctorRepository: IDoctorRepository) {}
+  private mapToPendingDoctorDTO(doc: any) {
+    return {
+      _id: doc._id,
+      fullName: doc.fullName,
+      isApproved: doc.isApproved,
+      isBlocked: doc.isBlocked,
+      registerNumber: doc.registerNumber,
+      isRejected: doc.isRejected,
+      documents: doc.documents,
+    };
+  }
 
   async execute(page: number, size: number, search: string) {
     const skip = (page - 1) * size;
@@ -17,10 +28,13 @@ export class FetchPendingDoctors {
         }
       : { isApproved: false };
 
-    const doctors = await this.doctorRepository.getDoctorsWithPagination(
+    const doctorsData = await this.doctorRepository.getDoctorsWithPagination(
       skip,
       limit,
       query
+    );
+    const doctors = doctorsData.map((doc) =>
+      this.mapToPendingDoctorDTO(doc)
     );
     const totalDoctors = await this.doctorRepository.countDoctors(query);
     const totalPages = Math.ceil(totalDoctors / size);
